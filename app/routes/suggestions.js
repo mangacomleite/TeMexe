90a1de1e8b1402e99b9dd0311759e141b680d525
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2016-12-16 20:42:19 -0200

    Create suggestions route

---
+++ app/routes/suggestions.js
@@ -0,0 +1,40 @@
+var fs = require( 'fs' );
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+
+module.exports = function(app) {
+
+  app.get( '/sugestoes', function( request, response ) {
+    response.render( 'suggestions/form' );
+  });
+
+  app.post( '/sugestoes', multipartMiddleware, function( request, response ) {
+
+    var suggestion = request.body;
+    var connection = app.infra.connectionFactory();
+
+    fs.readFile( request.files.image.path, function ( err, data ) {
+
+      let extension = 'jpg';
+      if ( request.files.image.type == 'image/png' ) {
+        extension = 'png';
+      } else if ( request.files.image.type == 'image/jpeg' ) {
+        extension = 'jpg';
+      }
+
+      var path = "uploads/" + Date.now() + '.' + extension;
+      var newPath = __dirname + "/../public/" + path;
+
+      suggestion.image = path;
+
+      console.log( suggestion );
+
+      fs.writeFile(newPath, data, function (err) {
+        connection.query( 'insert into suggestions set ?', suggestion, function( errors, results ) {
+          response.redirect( '/' );
+        });
+      });
+    });
+
+  });
+};
