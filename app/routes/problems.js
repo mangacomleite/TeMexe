78ad2492f12ee78851a05e720b396a75e471ae9a
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2016-12-16 19:39:09 -0200

    Create the problem route register

---
+++ app/routes/problems.js
@@ -0,0 +1,39 @@
+var fs = require( 'fs' );
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+
+module.exports = function(app) {
+
+  app.get( '/problemas', function( request, response ) {
+    response.render( 'problems/form' );
+  });
+
+  app.post( '/problemas', multipartMiddleware, function( request, response ) {
+
+    var problem = request.body;
+    var connection = app.infra.connectionFactory();
+
+
+    fs.readFile( request.files.image.path, function ( err, data ) {
+
+      let extension;
+      if ( request.files.image.type == 'image/png' ) {
+        extension = 'png';
+      } else if ( request.files.image.type == 'image/jpeg' ) {
+        extension = 'jpg';
+      }
+
+      var path = "uploads/" + Date.now() + '.' + extension;
+      var newPath = __dirname + "/../public/" + path;
+
+      problem.image = path;
+
+      fs.writeFile(newPath, data, function (err) {
+        connection.query( 'insert into problems set ?', problem, function( errors, results ) {
+          response.redirect( '/' );
+        });
+      });
+    });
+
+  });
+};
--- script.sql
+++ script.sql
@@ -1,8 +1,8 @@
-CREATE TABLE `teMexe`.`new_table` (
+
+
+
+CREATE TABLE `teMexe`.`problems` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `description` VARCHAR(1000) NULL,
   `image` VARCHAR(1000) NULL,
   PRIMARY KEY (`id`));
-
-  ALTER TABLE `teMexe`.`new_table`
-  RENAME TO  `teMexe`.`complaint` ;
